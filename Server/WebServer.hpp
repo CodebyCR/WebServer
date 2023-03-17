35be6bc619616ce1c9e98b69028b0d7c5de48b14
CR <44606665+CodebyCR@users.noreply.github.com>
2023-03-17 21:18:12 +0100

    Improve WebServer.hpp

--- Server/WebServer.hpp
+++ Server/WebServer.hpp
@@ -13,6 +13,8 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 
+#include "../API/Rest.hpp"
+
 class WebServer {
 private:
     int port_;
@@ -33,7 +35,7 @@ public:
         server_socket_ = socket(AF_INET, SOCK_STREAM, 0);
 
         // Server-Adresse festlegen
-        struct sockaddr_in server_address;
+        struct sockaddr_in server_address{};
         server_address.sin_family = AF_INET;
         server_address.sin_port = htons(port_);
         if (ip_ == nullptr) {
@@ -41,6 +43,7 @@ public:
         } else {
             inet_pton(AF_INET, ip_, &(server_address.sin_addr));
         }
+        std::cout << "Server Address: " << inet_ntoa(server_address.sin_addr) << std::endl;
 
         // Socket an die Adresse binden (+1 für std::boolalpha)
         bool isBound = 1 + bind(server_socket_, (struct sockaddr *)&server_address, sizeof(server_address));
@@ -48,6 +51,8 @@ public:
 
         // Socket in den Listenmodus versetzen
         listen(server_socket_, 5);
+
+        const auto api = RestApi();
 
         // Auf Anfragen von Clients warten
         while (isRunning_) {
@@ -57,32 +62,40 @@ public:
             char buffer[1024];
             read(client_socket, buffer, 1024);
 
-            const std::string css = " text-align: center;"
-                                    " margin-top: 100px;"
-                                    " font-size: 50px;"
-                                    " font-family: sans-serif;"
-                                    " background-image:linear-gradient(90deg, purple, blue, cyan);"
-                                    " -webkit-background-clip: text;"
-                                    " -webkit-text-fill-color: transparent;";
+//            const std::string css = " text-align: center;"
+//                                    " margin-top: 100px;"
+//                                    " font-size: 50px;"
+//                                    " font-family: sans-serif;"
+//                                    " background-image:linear-gradient(90deg, purple, blue, cyan);"
+//                                    " -webkit-background-clip: text;"
+//                                    " -webkit-text-fill-color: transparent;";
+//
+//            // Response
+//            std::ostringstream response;
+//            response << "HTTP/1.1 200 OK\r\n";
+//            response << "Content-Type: text/html\r\n";
+//            response << "\r\n";
+//            response << "<html>"
+//                            "<body>"
+//                                "<div style=\"" << css << "\">"
+//                                    "<h1>Hello from C++ Server!</h1>"
+//                                    "<div style=\" text-align: right; margin-right: 10%;\">"
+//                                        "<a  href=\"https://github.com/CodebyCR\"> My GitHub </a>"
+//                                    "</div>"
+//                                "</div>"
+//                            "</body>"
+//                        "</html>";
 
-            // Response
-            std::ostringstream response;
-            response << "HTTP/1.1 200 OK\r\n";
-            response << "Content-Type: text/html\r\n";
-            response << "\r\n";
-            response << "<html>"
-                            "<body>"
-                                "<div style=\"" << css << "\">"
-                                    "<h1>Hello from C++ WebServer!</h1>"
-                                    "<div style=\" text-align: right; margin-right: 10%;\">"
-                                        "<a  href=\"https://github.com/CodebyCR\"> My GitHub </a>"
-                                    "</div>"
-                                "</div>"
-                            "</body>"
-                        "</html>";
+
+
+
+            // REST-API-Aufruf behandeln
+            std::string response = api.handle_request(buffer);
+
+            auto total_response = response;
 
             // send Client response
-            write(client_socket, response.str().c_str(), response.str().length());
+            write(client_socket, total_response.c_str(), total_response.length());
 
 
             close(client_socket);
